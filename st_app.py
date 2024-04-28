13cbbccc52dff3302f72b887b1db3fa87a512512
MOXHAN <maxim.hansen2000@gmail.com>
2024-04-28 11:38:24 +0200

    Initial commit

---
+++ st_app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import ollama
+from typing import Dict, Generator
+import threading
+import queue
+import pyaudio
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+#################### Audio Player Class ####################
+
+class AudioPlayer(threading.Thread):
+    def __init__(self, client):
+        super().__init__()
+        self.client = client
+        self.queue = queue.Queue()
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            text = self.queue.get()
+            if text is None:
+                break
+            self.play_audio(text)
+            self.queue.task_done()
+
+    def play_audio(self, text):
+        player_stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
+
+        with self.client.audio.speech.with_streaming_response.create(
+            model="tts-1",
+            voice="alloy",
+            response_format="pcm",
+            speed=1.0,
+            input=text,
+        ) as response:
+            for chunk in response.iter_bytes(chunk_size=1024):
+                player_stream.write(chunk)
+
+        player_stream.close()
+
+    def add_to_queue(self, text):
+        self.queue.put(text)
+
+#################### Ollama Generator Function ####################
+
+def ollama_generator(audio_player, messages: Dict) -> Generator:
+    stream = ollama.chat(
+        model="llama3", messages=messages, stream=True)
+    buffer_txt = ""  # Buffer to store text before sending to TTS
+    for chunk in stream:
+        # Buffer the message content
+        buffer_txt += chunk['message']['content']
+        # Check for full stops or other suitable points to split the text
+        if buffer_txt and (buffer_txt.endswith(('.', '!', '?')) or chunk.get("done", False)):
+            audio_player.add_to_queue(buffer_txt)  # Add buffered text to TTS queue
+            buffer_txt = ""  # Reset buffer after sending to TTS
+
+        yield chunk['message']['content']
+
+#################### Streamlit code ####################
+
+st.title("Llocal LLama")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "client" not in st.session_state:
+    load_dotenv()
+    st.session_state.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+if "audio_player" not in st.session_state:
+    st.session_state.audio_player = AudioPlayer(client=st.session_state.client)
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = st.write_stream(ollama_generator(st.session_state.audio_player, st.session_state.messages))
+        
+        st.session_state.messages.append({"role": "assistant", "content": response})
